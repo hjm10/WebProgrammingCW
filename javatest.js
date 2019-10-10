0f7ca0f9946bce3cd49ced1be56fdf5e259e0f14
jsblades <47464579+jsblades@users.noreply.github.com>
2019-10-10 15:33:21 +0100

    Update javatest.js
    changed so that code references the knight player for keyboard controls and jumping trial code

--- javatest.js
+++ javatest.js
@@ -1,7 +1,7 @@
 /*adding comments next*/
 var objImage= null;
 	function init(){
-		objImage=document.getElementById("monster");				
+		objImage=document.getElementById("knight");				
 		objImage.style.position='relative';
 		objImage.style.left='0px';
 		objImage.style.top='0px';
@@ -41,41 +41,41 @@ controller ={
 
 loop = function(){ //merging controller logic with physics. 
 	
-	if(controller.up && rectangle.jumping == false){ //jumping physics
+	if(controller.up && knight.jumping == false){ //jumping physics
 		//up key presses, rectangle not jumping, want it to jump
-		rectangle.y_velocity -=20;//send rectangle shooting upwards 
-		rectangle.jumping = true;
+		knight.y_velocity -=20;//send rectangle shooting upwards 
+		knight.jumping = true;
 		
 	}
 	
 	if (controller.left){//movement code for left and right keypressed events 
-		rectangle.x_velocity -=0.5;//left key, subtract .5 to x velocity
+		knight.x_velocity -=0.5;//left key, subtract .5 to x velocity
 	}
 	
 	if(controller.right){
-		rectangle.x_velocity +=0.5;//right key, add .5 to x velocity. 
+		knight.x_velocity +=0.5;//right key, add .5 to x velocity. 
 	}
 	
-	rectangle.y_velocity +=1.5; //simulating gravity, adds 1.5 on every frame of y velocity animation, makes rectangle fall
-	rectangle.x += rectangle.x_velocity;//adds x velocity to current position
-	rectangle.y += rectangle.y_velocity;//adds y velocity to current position
-	rectangle.x_velocity *= 0.9; //friction, effect of speed, keeps speed equal. 
-	rectangle.y_velocity *= 0.9; //friction
+	knight.y_velocity +=1.5; //simulating gravity, adds 1.5 on every frame of y velocity animation, makes rectangle fall
+	knight.x += knight.x_velocity;//adds x velocity to current position
+	knight.y += knight.y_velocity;//adds y velocity to current position
+	knight.x_velocity *= 0.9; //friction, effect of speed, keeps speed equal. 
+	knight.y_velocity *= 0.9; //friction
 	
 	//if player is falling below the floor line, this will prevent it from doing so. 
-	if (rectangle.y > 180 - 16 - 32){
-		rectangle.jumping = false;//so we can jump again
-		rectangle.y = 180 - 16 - 32;
-		rectangle.y_velocity = 0;//smooth velocity
+	if (knight.y > 180 - 16 - 32){
+		knight.jumping = false;//so we can jump again
+		knight.y = 180 - 16 - 32;
+		knight.y_velocity = 0;//smooth velocity
 	}
 	
 	//if rectangle is going off the left of the screen
-	if(rectangle.x < -32){
-		rectangle.x = 320;
+	if(knight.x < -32){
+		knight.x = 320;
 		
 	}
-	else if (rectangle.x > 320){ //if rectangle goes past right of the screen, teleported over to the left side of the screen
-		rectangle.x = -32;
+	else if (knight.x > 320){ //if rectangle goes past right of the screen, teleported over to the left side of the screen
+		knight.x = -32;
 	}
 	
 //call update when the browser is ready to draw again
