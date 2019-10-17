7253244f78415ebe369fb90c99828c8e6f1d8490
Jessica <jsb4@hw.ac.uk>
2019-10-17 12:25:05 +0100

    changed character to move to knight from monster

--- javatest.js
+++ javatest.js
@@ -1,7 +1,7 @@
 /*adding comments next*/
 var objImage= null;
 	function init(){
-		objImage=document.getElementById("monster");				
+		objImage=document.getElementById("knight");				
 		objImage.style.position='relative';
 		objImage.style.left='0px';
 		objImage.style.top='0px';
@@ -12,12 +12,12 @@ var objImage= null;
             case 32: //spacebar
             jump();
             break;
-			case 37: //left arrow key
-				moveLeft();
-				break;
-			case 39: //right arrow key
-				moveRight();
-				break;
+			        case 37: //left arrow key
+				         moveLeft();
+				         break;
+			        case 39: //right arrow key
+				         moveRight();
+				         break;
 								
 		}
     }
