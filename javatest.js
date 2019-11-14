ebc1b63225b3e37491b92e159a87c9cd6e1ddd05
Jessica <jsb4@hw.ac.uk>
2019-11-14 12:59:58 +0000

    loading page

--- javatest.js
+++ javatest.js
@@ -47,3 +47,23 @@ function monsterRun(){
 	}
 setTimeout(monsterRun,10000);
 	document.onkeydown = move;
+
+	(function() {
+
+		var preload = document.getElementById("preload");
+		var loading = 0;
+		var id = setInterval(frame, 64);
+
+		function frame() {
+			if(loading == 100){
+				clearInterval(id);
+				window.open("welcome.html", "_self");
+			} else {
+				loading = loading + 1;
+				if(loading == 90){
+					preload.style.animation = "fadeout 1s ease";
+				}
+			}
+		}
+
+	})();
